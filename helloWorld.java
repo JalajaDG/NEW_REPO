d3618011ff471ca94328b959c20ed858fe31de93
JalajaDG <jalajagond@gmail.com>
2025-08-31 15:37:22 +0530

    edited helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,8 @@
 public class helloWorld {
     public static void main(String[] args) {
 	   System.out.println("***************\n");
-    	    System.out.println("Hello, World!");
+    	    System.out.println("Hello, World!\n");
+	    System.out.println("***************\n");
     }
 }
 
